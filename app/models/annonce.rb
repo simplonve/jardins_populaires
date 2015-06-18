7aa2831996d4122795428ae60ec023293caeb6d7
svey <stephane.vey@laposte.net>
2015-06-18 11:29:27 +0200

    affichage ordre croissant

--- app/models/annonce.rb
+++ app/models/annonce.rb
@@ -1,24 +1,27 @@
 class Annonce < ActiveRecord::Base
 	validates :titre, :adresse, :dispo, :quantite, presence: true
 	validates :code_postal, presence: true, length: { is: 5 }, :numericality => true
+
 	def self.search(cat, code_postal)
 		if is_tout?(cat)
 			if is_partout?(code_postal)
-				all
+				order(dispo: :asc)
 			else
-				all.where(code_postal: code_postal)
+				where(code_postal: code_postal).order(dispo: :asc)
 			end
 		else
 			if is_partout?(code_postal)
-				all.where(categorie: cat)
+				all.where(categorie: cat).order(dispo: :asc)
 			else
-				where(categorie: cat, code_postal: code_postal)
+				where(categorie: cat, code_postal: code_postal).order(dispo: :asc)
 			end			
 		end
  	end
+
  	def self.is_tout?(cat)
  		cat == 'Tout'
  	end
+
  	def self.is_partout?(code_postal)
  		code_postal == ''
  	end
